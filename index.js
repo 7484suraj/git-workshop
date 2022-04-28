a783a5d5970584b0f6ea5277f7dab6b4c67d6107
7484suraj <suraj@leewayhertz.com>
2022-04-28 10:50:11 +0530

    AfterGitWorkShop

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const gitWorkShop = () => {
     console.log("Understand, What is Git and Its basics - Repository, Branches, Commit, Pull, Push, Merge.")
 }
+const AfterGitWorkShop = () => {
+    console.log("New Advanture is waiting for you !!!!")
+}
 
 gitWorkShop()
+AfterGitWorkShop()
